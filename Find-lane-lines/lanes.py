5c0999a885c03ac0ad995a93b6760353c79ef537
wagdi <wagdimoqbil@gmail.com>
2020-04-22 23:22:48 +0200

    Find lines update

--- Find-lane-lines/lanes.py
+++ Find-lane-lines/lanes.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def canny(m):
@@ -9,8 +10,31 @@ def canny(m):
     return canny_im
 
 
+def interest_region(image):
+    height = image.shape[0]
+    tri = np.array([
+        [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, tri, 255)
+    masked_image = cv2.bitwise_and(image,mask)#
+    return masked_image
+
+def display_lines(image,lines):
+    line_image= np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2,y2), (255,0,0), 10)
+    return line_image
+
+
 image = cv2.imread('test_image.jpg')
 copy_im = np.copy(image)
 m = canny(copy_im)
-cv2.imshow('result', m)
+int_reg = interest_region(m)
+lines = cv2.HoughLinesP(int_reg,2,np.pi/180,100, np.array([]), minLineLength=40, maxLineGap=5)
+image_lines= display_lines(copy_im,lines)
+com_image =cv2.addWeighted(copy_im, 0.8, image_lines, 1, 1)
+cv2.imshow("result", com_image)
 cv2.waitKey(0)
