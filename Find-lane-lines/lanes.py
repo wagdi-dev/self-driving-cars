923ab9854592d00c101cc2adaa9590229f24dd68
wagdi <wagdimoqbil@gmail.com>
2020-04-22 10:35:17 +0200

    update finding lane lines

--- Find-lane-lines/lanes.py
+++ Find-lane-lines/lanes.py
@@ -1,5 +1,10 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('test_image.jpg')
-cv2.imshow('result', image)
+copy_im = np.copy(image)
+gray_im = cv2.cvtColor(copy_im, cv2.COLOR_BGR2GRAY)
+blur_im = cv2.GaussianBlur(gray_im, (5,5),0)
+canny_im =cv2.Canny(blur_im,50,150)
+cv2.imshow('result', canny_im)
 cv2.waitKey(0)
