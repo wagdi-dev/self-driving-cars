2d5b4a4bcfac9941e1265cfc1ae6d94447a41e87
wagdi <wagdimoqbil@gmail.com>
2020-04-22 11:16:12 +0200

    create canny function

--- Find-lane-lines/lanes.py
+++ Find-lane-lines/lanes.py
@@ -1,10 +1,16 @@
 import cv2
 import numpy as np
 
+
+def canny(m):
+    gray_im = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
+    blur_im = cv2.GaussianBlur(gray_im, (5, 5), 0)
+    canny_im = cv2.Canny(blur_im, 50, 150)
+    return canny_im
+
+
 image = cv2.imread('test_image.jpg')
 copy_im = np.copy(image)
-gray_im = cv2.cvtColor(copy_im, cv2.COLOR_BGR2GRAY)
-blur_im = cv2.GaussianBlur(gray_im, (5,5),0)
-canny_im =cv2.Canny(blur_im,50,150)
-cv2.imshow('result', canny_im)
+m = canny(copy_im)
+cv2.imshow('result', m)
 cv2.waitKey(0)
